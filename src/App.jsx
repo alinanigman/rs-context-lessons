e60a477dc5440b26c49a6236e4cc916948078e03
alinakassym <alinakassym@gmail.com>
2025-08-05 12:25:03 +0500

    feat: add change user

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,51 @@
+import { useState, useEffect } from "react";
 import { Header, UserBlock } from "./components";
 import { AppContext } from "./context";
 import styles from "./App.module.css";
 
-const getUserFromServer = () => ({
-  id: "userid123456789",
-  name: "John Doe",
-  age: 32,
-  email: "johndoe@gmail.com",
-  phone: "+95551234567",
-});
+const getUserFromServer = (id) => {
+  if (id === 1) {
+    return {
+      id: "userid123456789",
+      name: "John Doe",
+      age: 32,
+      email: "johndoe@gmail.com",
+      phone: "+95551234567",
+    };
+  }
+  return {
+    id: "userid22222222",
+    name: "Bob Dylan",
+    age: 48,
+    email: "bob@gmail.com",
+    phone: "+95550981256",
+  };
+};
 
 export const App = () => {
-  const userData = getUserFromServer();
+  const [userData, setUserData] = useState();
+
+  useEffect(() => {
+    const requestData = getUserFromServer(1);
+    setUserData(requestData);
+  }, []);
+
+  const onChangeUser = () => {
+    const requestData = getUserFromServer(2);
+    setUserData(requestData);
+  };
+
+  if (!userData) {
+    return <>Oops :/</>;
+  }
+
   return (
     <AppContext value={userData}>
       <div className={styles.App}>
         <Header />
         <hr />
         <UserBlock />
+        <button onClick={onChangeUser}>Change user</button>
       </div>
     </AppContext>
   );
