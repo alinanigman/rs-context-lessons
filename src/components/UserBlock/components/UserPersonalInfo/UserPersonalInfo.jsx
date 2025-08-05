cd8c0da3e6cce4214a210de33875271cb3d3bf1d
alinakassym <alinakassym@gmail.com>
2025-08-05 11:59:24 +0500

    feat(UserPersonalInfo): use AppContext

--- src/components/UserBlock/components/UserPersonalInfo/UserPersonalInfo.jsx
+++ src/components/UserBlock/components/UserPersonalInfo/UserPersonalInfo.jsx
@@ -1,4 +1,8 @@
-export const UserPersonalInfo = ({ name, age }) => {
+import { AppContext } from "@/context";
+import { use } from "react";
+
+export const UserPersonalInfo = () => {
+  const { name, age } = use(AppContext);
   return (
     <div>
       <p>Name: {name}</p>
